baf237a8ae9abdb5f36e31d85a3b59b585558766
belkins <belibov.nikolay@gmail.com>
2019-10-01 11:37:48 +0300

    modify form

--- src/blocks/modules/footer/config.js
+++ src/blocks/modules/footer/config.js
@@ -79,105 +79,105 @@ export default {
                     }
                 ]
             },
-            {
-                "featureType": "poi",
-                "elementType": "geometry",
-                "stylers": [
-                    {
-                        "color": "#f5f5f5"
-                    },
-                    {
-                        "lightness": 21
-                    }
-                ]
-            },
-            {
-                "featureType": "poi.park",
-                "elementType": "geometry",
-                "stylers": [
-                    {
-                        "color": "#dedede"
-                    },
-                    {
-                        "lightness": 21
-                    }
-                ]
-            },
-            {
-                "elementType": "labels.text.stroke",
-                "stylers": [
-                    {
-                        "visibility": "on"
-                    },
-                    {
-                        "color": "#ffffff"
-                    },
-                    {
-                        "lightness": 16
-                    }
-                ]
-            },
-            {
-                "elementType": "labels.text.fill",
-                "stylers": [
-                    {
-                        "saturation": 36
-                    },
-                    {
-                        "color": "#333333"
-                    },
-                    {
-                        "lightness": 40
-                    }
-                ]
-            },
-            {
-                "elementType": "labels.icon",
-                "stylers": [
-                    {
-                        "visibility": "off"
-                    }
-                ]
-            },
-            {
-                "featureType": "transit",
-                "elementType": "geometry",
-                "stylers": [
-                    {
-                        "color": "#f2f2f2"
-                    },
-                    {
-                        "lightness": 19
-                    }
-                ]
-            },
-            {
-                "featureType": "administrative",
-                "elementType": "geometry.fill",
-                "stylers": [
-                    {
-                        "color": "#fefefe"
-                    },
-                    {
-                        "lightness": 20
-                    }
-                ]
-            },
-            {
-                "featureType": "administrative",
-                "elementType": "geometry.stroke",
-                "stylers": [
-                    {
-                        "color": "#fefefe"
-                    },
-                    {
-                        "lightness": 17
-                    },
-                    {
-                        "weight": 1.2
-                    }
-                ]
-            }
+        //     {
+        //         "featureType": "poi",
+        //         "elementType": "geometry",
+        //         "stylers": [
+        //             {
+        //                 "color": "#f5f5f5"
+        //             },
+        //             {
+        //                 "lightness": 21
+        //             }
+        //         ]
+        //     },
+        //     {
+        //         "featureType": "poi.park",
+        //         "elementType": "geometry",
+        //         "stylers": [
+        //             {
+        //                 "color": "#dedede"
+        //             },
+        //             {
+        //                 "lightness": 21
+        //             }
+        //         ]
+        //     },
+        //     {
+        //         "elementType": "labels.text.stroke",
+        //         "stylers": [
+        //             {
+        //                 "visibility": "on"
+        //             },
+        //             {
+        //                 "color": "#ffffff"
+        //             },
+        //             {
+        //                 "lightness": 16
+        //             }
+        //         ]
+        //     },
+        //     {
+        //         "elementType": "labels.text.fill",
+        //         "stylers": [
+        //             {
+        //                 "saturation": 36
+        //             },
+        //             {
+        //                 "color": "#333333"
+        //             },
+        //             {
+        //                 "lightness": 40
+        //             }
+        //         ]
+        //     },
+        //     {
+        //         "elementType": "labels.icon",
+        //         "stylers": [
+        //             {
+        //                 "visibility": "off"
+        //             }
+        //         ]
+        //     },
+        //     {
+        //         "featureType": "transit",
+        //         "elementType": "geometry",
+        //         "stylers": [
+        //             {
+        //                 "color": "#f2f2f2"
+        //             },
+        //             {
+        //                 "lightness": 19
+        //             }
+        //         ]
+        //     },
+        //     {
+        //         "featureType": "administrative",
+        //         "elementType": "geometry.fill",
+        //         "stylers": [
+        //             {
+        //                 "color": "#fefefe"
+        //             },
+        //             {
+        //                 "lightness": 20
+        //             }
+        //         ]
+        //     },
+        //     {
+        //         "featureType": "administrative",
+        //         "elementType": "geometry.stroke",
+        //         "stylers": [
+        //             {
+        //                 "color": "#fefefe"
+        //             },
+        //             {
+        //                 "lightness": 17
+        //             },
+        //             {
+        //                 "weight": 1.2
+        //             }
+        //         ]
+        //     }
         ],
     }
 }
